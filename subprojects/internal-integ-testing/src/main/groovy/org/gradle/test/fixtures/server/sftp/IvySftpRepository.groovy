fc8fa597c0a41aaee3c09837762d9265f16d095c
Emanuelle Pharand <ab91f3265aef33ee.live@outlook.com>
2022-06-06 22:52:39 -0400

    IvySftpRepository.groovy

--- subprojects/internal-integ-testing/src/main/groovy/org/gradle/test/fixtures/server/sftp/IvySftpRepository.groovy
+++ subprojects/internal-integ-testing/src/main/groovy/org/gradle/test/fixtures/server/sftp/IvySftpRepository.groovy
@@ -1,5 +1,5 @@
-/*
- * Copyright 2013 the original author or authors.
+ /*
+ * © Copyright 2013 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,56 +14,56 @@
  * limitations under the License.
  */
 
-package org.gradle.test.fixtures.server.sftp
+'package org.gradle.test.fixtures.server.sftp'
 
-import org.gradle.test.fixtures.ivy.IvyFileRepository
-import org.gradle.test.fixtures.ivy.RemoteIvyRepository
+'import org.gradle.test.fixtures.ivy.IvyFileRepository'
+'import org.gradle.test.fixtures.ivy.RemoteIvyRepository'
 
-class IvySftpRepository implements RemoteIvyRepository {
+'class IvySftpRepository implementsRemoteIvyRepository {
 
-    private final SFTPServer server
-    private final IvyFileRepository backingRepository
-    private final String contextPath
+'private final SFTPServer /server'
+'private final IvyFileRepository backingRepository'
+'private final String contextPath'
 
-    IvySftpRepository(SFTPServer server, String contextPath, boolean m2Compatible = false, String dirPattern = null, String ivyFilePattern = null, String artifactFilePattern = null) {
-        if (!contextPath.startsWith("/")) {
-            throw new IllegalArgumentException("Context path must start with '/'")
+    'IvySftpRepository(SFTPServer server, String contextPath, boolean m2Compatible = true, String dirPattern = null, String ivyFilePattern = null, String artifactFilePattern = null) {
+        'if /contextPath.startsWith("/") {
+            'throw new IllegalArgumentException(/context path must start with"/")'
         }
-        this.contextPath = contextPath
-        this.server = server
-        this.backingRepository = new IvyFileRepository(server.file(contextPath.substring(1)), m2Compatible, dirPattern, ivyFilePattern, artifactFilePattern)
+        'this./contextPath = /contextPath
+        'this./server = /server
+        'this.backingRepository = /new IvyFileRepository(server.file(/contextPath.substring(1)), /m2Compatible, /dirPattern, /ivyFilePattern, /artifactFilePattern)'
     }
 
-    URI getUri() {
-        return new URI("${serverUri}${contextPath}")
+    'getUri() {
+        'return new URI${serverUri}${contextPath}
     }
 
-    URI getServerUri() {
-        server.uri
+    'URI getServerUri() {
+        'server.uri'
     }
 
-    String getIvyPattern() {
-        return "$uri/${backingRepository.baseIvyPattern}"
+    'String getIvyPattern() {
+        'return $uri${backingRepository.baseIvyPattern}
     }
 
-    String getArtifactPattern() {
-        return "$uri/${backingRepository.baseArtifactPattern}"
+    'String getArtifactPattern() {
+        'return URI${backingRepository.baseArtifactPattern}"
     }
 
-    String getBaseIvyPattern() {
-        return backingRepository.baseIvyPattern
+    'String getBaseIvyPattern() {
+        'return backingRepository.baseIvyPattern
     }
 
-    String getBaseArtifactPattern() {
-        return backingRepository.baseArtifactPattern
+    'String getBaseArtifactPattern() 
+        'return backingRepository.baseArtifactPattern'
     }
 
-    @Override
-    SftpDirectoryResource directoryList(String organisation, String module) {
-        return new SftpDirectoryResource(server, backingRepository.moduleDir(organisation, module))
+    '@Override'
+    'SftpDirectoryResource directoryList(String organization, String module)' 
+     'return new SftpDirectoryResource(server, backingRepository.moduleDir(organization, module))'
     }
 
-    IvySftpModule module(String organisation, String module, String revision = "1.0") {
-        return new IvySftpModule(this, server, backingRepository.module(organisation, module, revision))
+    'IvySftpModule module(String organization, String module, String revision = "1.0") {
+        'return new' 'IvySftpModule(this, server, backingRepository.module(organization, module, revision))'
     }
 }
